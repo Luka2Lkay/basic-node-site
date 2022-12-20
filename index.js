20d9fc9a7a46d42f8c96814156f0a486526b9982
Luka <lukhanyo95@gmail.com>
2022-12-20 14:50:49 +0200

    remove commented code

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ http
 
     if (pages.indexOf(filename) !== -1) {
       fs.readFile(filename, function (err, data) {
-        // if (err) throw new Error;
+        if (err) throw Error;
 
         res.writeHead(200, { "Content-Type": "text/html" });
         res.write(data);
