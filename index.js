785d0e314ae0f3d5999170cd046170b676b8128b
Luka <lukhanyo95@gmail.com>
2022-12-22 21:49:40 +0200

    reduce lines of code and add style

--- index.js
+++ index.js
@@ -1,29 +1,15 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 const { readFile } = require("fs").promises;
 
-const port = process.env.PORT || 8080;
-const hostname = "0.0.0.0";
-
-app.get("/", async (req, res) => {
-  res.send(await readFile("index.html", "utf8"));
-});
-
-app.get("/index.html", async (req, res) => {
-  res.send(await readFile("index.html", "utf8"));
-});
-
-app.get("/about.html", async (req, res) => {
-  res.send(await readFile("about.html", "utf8"));
-});
-
-app.get("/contact-me.html", async (req, res) => {
-  res.send(await readFile("contact-me.html", "utf8"));
-});
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(async (req, res) => {
   res.status(404).send(await readFile("404.html", "utf8"));
 });
+
+const port = process.env.PORT || 8080;
+const hostname = "0.0.0.0";
 
 app.listen(port, hostname, () => {
   console.log(`http://${hostname}:${port}`);
